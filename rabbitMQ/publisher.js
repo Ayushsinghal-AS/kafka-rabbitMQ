c6acb1bda0b5346fd3a66801a329c6bac104dba4
ayushs <ayushs@spanidea.com>
2024-04-12 18:06:13 +0530

    rabbitMQ

---
+++ rabbitMQ/publisher.js
@@ -0,0 +1,22 @@
+const amqp = require("amqplib");
+
+const msg = {number: process.argv[2]}
+
+connect();
+async function connect() {
+
+    try {
+        const amqpServer = "amqp://127.0.0.1:5672"
+        const connection = await amqp.connect(amqpServer)
+        const channel = await connection.createChannel();
+        await channel.assertQueue("jobs");
+        await channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)))
+        console.log(`Job sent successfully ${msg.number}`);
+        await channel.close();
+        await connection.close();
+    }
+    catch (ex){
+        console.error(ex)
+    }
+
+}
